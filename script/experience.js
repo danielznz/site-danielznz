d6a0f94c9b836eaa69dc063a2e1d70d49bdb8f8c
Daniel Teixeira <daniel.teixeira.corporativo@gmail.com>
2026-04-22 08:49:55 -0300

    texto-experiencia

--- script/experience.js
+++ script/experience.js
@@ -1,28 +1,16 @@
 const experiences = [
   {
-    title: "Desenvolvedor Front End JR",
-    company: "Tucupy Tecnologia",
-    date: "NOV 2024 – ATUALMENTE",
-    description: `Atuo na área de desenvolvimento front end, focado na implementação e
-      melhoria de interfaces e funcionalidades em projetos internos. Meu trabalho
-      envolve colaboração com a equipe e integração com serviços internos da empresa.`
+    title: "Estágio - Desenvolvedor de Software",
+    company: "BRVANT / BRV UAV & Flight Systems",
+    date: "JAN 2026 – ATUALMENTE",
+    description: `Atuo como estagiário em desenvolvimento de software na BRVANT, trabalhando principalmente com tecnologias web como HTML, CSS e JavaScript.
+Também tenho experiência com desenvolvimento embarcado, utilizando C++ e C#, além de participar e apoiar demandas relacionadas à Inteligência Artificial.`
   },
   {
-    title: "Estagiário de desenvolvimento fullstack",
-    company: "Agência Virtú Marketing Digital",
-    date: "MAI 2024 – SET 2024",
-    description: `Fui responsável por um sistema de notícias, onde no frontend era utilizado React
-      para consumir uma API feita em Node.js. O sistema consistia em diversas páginas com
-      tópicos diferentes de notícias, além de uma área administrativa. Refatorei toda a API,
-      melhorando o tempo de resposta, o SEO e o desempenho geral do sistema.`
-  },
-  {
-    title: "Estagiário de T.I",
-    company: "Medcare Consultoria e Serviços Hospitalares LTDA",
-    date: "MAR 2023 – ABR 2024",
-    description: `Trabalhava com montagem, manutenção, formatação e instalação de programas
-      em computadores, além de suporte direto aos usuários. Também fazia instalação,
-      configuração e troca de suprimentos de impressoras.`
+    title: "Monitor",
+    company: "Fatec Itaquera",
+    date: "AGO 2023 – DEZ 2023",
+    description: `Fui membro do Programa de Monitoria na Fatec Itaquera, onde auxiliei alunos do 1º semestre do curso de Desenvolvimento de Software Multiplataforma na disciplina de Design Digital.`
   }
 ];
 
