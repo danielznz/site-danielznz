025dcce6792adad3e7c6c80995d2f9573eebd248
Daniel Teixeira <daniel.teixeira.corporativo@gmail.com>
2026-04-22 08:38:20 -0300

    textos-educação

--- script/education.js
+++ script/education.js
@@ -3,21 +3,21 @@ const educations = [
     institution: "FATEC Itaquera",
     course: "Desenvolvimento de Software Multiplataforma",
     period: "2023 – 2026",
-    description: "Graduação focada em desenvolvimento de software, estruturas de dados, banco de dados e engenharia de sistemas.",
+    description: "Formação focada no desenvolvimento de aplicações desktop, web e mobile. Durante o curso, adquiri conhecimentos sólidos em diversas tecnologias e ferramentas, aplicando-os em projetos práticos ao longo da graduação.",
     country: "br"
   },
   {
     institution: "Instituto Politécnico da Maia",
     course: "Tecnologias de Informação, Web e Multimédia",
     period: "2025",
-    description: "Formação prática em desenvolvimento web moderno com foco em React, Node.js, APIs REST e boas práticas de código.",
+    description: "Participei do programa de mobilidade internacional da FATEC (CPS) em 2025, tendo a oportunidade de cursar um semestre no IPMAIA, em Portugal. Essa experiência contribuiu significativamente para meu crescimento profissional, permitindo contato com novas tecnologias, ferramentas e diferentes culturas.",
     country: "pt"
   },
     {
     institution: "ETEC Ferraz de Vasconcelos",
     course: "Programação de Jogos Digitais",
     period: "2020 – 2022",
-    description: "Formação prática em desenvolvimento web moderno com foco em React, Node.js, APIs REST e boas práticas de código.",
+    description: "Cursei o ensino técnico em Jogos Digitais integrado ao Ensino Médio em 2020. Essa formação foi fundamental para minha introdução na área de tecnologia, despertando meu interesse por desenvolvimento e lógica de programação.",
     country: "br"
   }
 ];
